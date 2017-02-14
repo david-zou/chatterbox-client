21aaa993bec476237667c7e7e953ed005cb9567f
MKS-Student <mks.student@hackreactor.com>
2017-02-14 11:41:15 -0800

    Added create room function (partially working, need to filter out messages still), working on send and submit. Made a few changes to main CSS stylesheet.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,6 +4,9 @@ class App {
 
   constructor() {
     this.server = 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages';
+    this.friendList = {};
+    this.roomname = '';
+    this.username = '';
   }
 
   init() {
@@ -25,6 +28,7 @@ class App {
         console.error('chatterbox: Failed to send message', data);
       }
     });
+    // this.fetch();
   }
 
   fetch() {
@@ -35,14 +39,17 @@ class App {
       contentType: 'application/json',
       success: function (data) {
         console.log('MESSAGE RECEIVED', data);
-        for (var index = 0; index < data.results.length; index++) {
+        // for (var index = 0; index < data.results.length; index++) {
+        for (var index = data.results.length-1; index >= 0; index--) {
           var msg = { username: '', text: '', roomname: ''};
           msg.username = data.results[index].username;
           msg.text = data.results[index].text;
           msg.roomname = data.results[index].roomname;
           app.renderMessage(msg);
           app.renderRoom(msg.roomname);
+          app.roomname = msg.roomname;
         }
+
       },
       error: function (data) {
         // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -56,31 +63,57 @@ class App {
   }
 
   renderMessage(message) {
-    $('#main').append('<div class="username">' + message.username + '</div>');
-    $('#main').find('.username').on('click', function(){
+    $('#main').append('<div class="username">' + message.username + /*'<br>' + message.text +*/ '</div>');
+    $/*('#main').find('.username')*/('.username').on('click', function() {
+      // this should only run once per click
       app.handleUsernameClick();
     });
-    $('#chats').append("<div class='chat'>" + message.text + '</div>');
+    $('#main').find('.username').hide();
+    $('#chats').append('<div class="username ' + message.username + '">' + message.username + '<br>' + message.text + '</div>');
+    $('#chats').find('.' + message.username).on('click', function() {
+      app.handleUsernameClick();
+    });
+    // $('#chats').append("<div class='chat " + message.roomname + "'>" + message.text + '</div>');
   }
 
   renderRoom(string) {
     var exists = ($('#roomSelect option[value=' + string + ']').length !== 0);
     if (!exists) {
       $('#roomSelect').append('<option value="' + string + '">' + string + '</option>');
-      $('#roomSelect').change(function() {
-        var room = $(this).val();
-        alert(room);
-        // call a function that filters the chat by selected room
-      });
+      // $('#roomSelect').change(function() {
+      //   var room = $(this).val();
+      //   alert(room);
+      //   if (room === 'create') {
+      //     var newRoom = prompt('Please enter a room name.');
+      //     app.renderRoom(newRoom);
+      //   }
+
+      //   this.roomname = room;
+      //   // call a function that filters the chat by selected room
+      //   // change user's this.room to that room
+      // });
     }
   }
 
   handleUsernameClick() {
     console.log('handleUsernameClick called');
+      // add as friend 
+
   }
 
   handleSubmit(){
+    // create a message object
+    // grab current user's name
+    // grab current user's room
+    // grab current user's message in text input field
+    // package that data the object
+    // invoke app.send(message object)
     console.log('handleSubmit called');
+    var msg = { username: '', text: '', roomname: ''};
+    msg.username = app.username;
+    msg.text = $('#send').find('input').val();
+    msg.roomname = app.roomname;
+    app.send(msg);
   }
 
 };
@@ -90,15 +123,31 @@ let app = new App;
 //   JQUERY DOCUMENT ------------------
 $(document).ready(function() { 
 
-  app.init(); 
+  app.init();
 
-  $('#send .submit').on('submit', function(){
+  app.username = window.location.search.slice(10);
+
+  $('#send').submit(function(event){
     console.log('i am clicked');
     app.handleSubmit();
+    return;
   });
 
   $('body').append("<div class='chat'></div>");
   console.log($('#main .username'));
   $('.chat').append($(".username").find('div').text() + " " + $("#chats").text());
+
+  $('#roomSelect').change(function() {
+    var room = $(this).val();
+    alert(room);
+    if (room === 'create') {
+      var newRoom = prompt('Please enter a room name.');
+      app.renderRoom(newRoom);
+    }
+
+    this.roomname = room;
+    // call a function that filters the chat by selected room
+    // change user's this.room to that room
+  });
 
 });
